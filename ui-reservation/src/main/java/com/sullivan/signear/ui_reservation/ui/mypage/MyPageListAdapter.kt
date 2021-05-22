48e1c3ee3a489765f81cabffb7dcbba6be3df3a9
Bentley Park <junggil.park@sovoro.kr>
2021-05-22 23:55:32 +0900

    #4: 마이 페이지 > 의견 보내기 작업

--- ui-reservation/src/main/java/com/sullivan/signear/ui_reservation/ui/mypage/MyPageListAdapter.kt
+++ ui-reservation/src/main/java/com/sullivan/signear/ui_reservation/ui/mypage/MyPageListAdapter.kt
@@ -1,11 +1,15 @@
 package com.sullivan.signear.ui_reservation.ui.mypage
 
 import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.jaredrummler.materialspinner.BuildConfig.VERSION_NAME
+import com.sullivan.sigenear.ui_reservation.BuildConfig
 import com.sullivan.sigenear.ui_reservation.R
 import com.sullivan.sigenear.ui_reservation.databinding.ItemMypageBinding
 import com.sullivan.signear.common.navigator.LoginNavigator
@@ -28,9 +32,27 @@ class MyPageListAdapter(
                         "지난 예약" -> it.findNavController()
                             .navigate(R.id.action_myPageFragment_to_previousReservationFragment)
                         "로그아웃" -> showDialog(it.context)
+                        "의견 남기기" -> sendEmail(it.context)
                     }
                 }
             }
+        }
+
+        private fun sendEmail(context: Context) {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra(
+                Intent.EXTRA_EMAIL,
+                arrayOf("sullivan_developer@signear.com"))
+            intent.putExtra(
+                Intent.EXTRA_SUBJECT,
+                "Signear에게 의견 보내기"
+            )
+            intent.putExtra(
+                Intent.EXTRA_TEXT,
+                "앱 버전 (AppVersion): ${BuildConfig.VERSION_NAME}\n기기명 (Device):\n안드로이드 OS (Android OS): ${Build.VERSION.RELEASE + ".0"}\n내용 (Content):\n"
+            )
+            context.startActivity(intent)
         }
 
         private fun showDialog(context: Context) {
