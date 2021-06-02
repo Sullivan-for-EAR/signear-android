1da1ddb4e0df5b22eedb6b5f808e4db479d47ee0
bentleypark <p2c2kbf@gmail.com>
2021-06-02 10:10:53 +0900

    bump gson version to 2.8.7

--- buildSrc/src/main/java/Dep.kt
+++ buildSrc/src/main/java/Dep.kt
@@ -152,7 +152,7 @@ object Dep {
     object Retrofit {
         private const val version = "2.9.0"
         const val retrofit = "com.squareup.retrofit2:retrofit:$version"
-        const val gson = "com.google.code.gson:gson:2.8.6"
+        const val gson = "com.google.code.gson:gson:2.8.7"
         const val converterGson = "com.squareup.retrofit2:converter-gson:$version"
     }
 
