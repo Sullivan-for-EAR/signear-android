8e10979bea506524d0626ecf2374dc09703cc9bb
Bentley <bentley@cleaninglab.co.kr>
2021-07-15 01:52:13 +0900

    #10: api 주소 변경 및 SSL 인증서 오류 수정

--- data/src/main/java/com/sullivan/signear/data/di/RetrofitModule.kt
+++ data/src/main/java/com/sullivan/signear/data/di/RetrofitModule.kt
@@ -14,6 +14,14 @@ import okhttp3.Request
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.security.SecureRandom
+import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
+import javax.net.ssl.HostnameVerifier
+import javax.net.ssl.SSLContext
+import javax.net.ssl.SSLSession
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 @Module
 @InstallIn(ViewModelComponent::class)
@@ -22,22 +30,60 @@ object RetrofitModule {
     @ViewModelScoped
     @Provides
     fun provideOkhttpClient(authInterceptor: AuthInterceptor): OkHttpClient {
-        val logger = HttpLoggingInterceptor()
-        val httpClient: OkHttpClient.Builder = OkHttpClient.Builder()
-        with(httpClient) {
-            logger.setLevel(HttpLoggingInterceptor.Level.HEADERS)
-            logger.setLevel(HttpLoggingInterceptor.Level.BODY)
-            addInterceptor(logger)
-            addInterceptor(authInterceptor)
+        return try {
+            // Create a trust manager that does not validate certificate chains
+            val trustAllCerts = arrayOf<TrustManager>(
+                object : X509TrustManager {
+                    @Throws(CertificateException::class)
+                    override fun checkClientTrusted(
+                        chain: Array<X509Certificate>,
+                        authType: String
+                    ) {
+                    }
+
+                    @Throws(CertificateException::class)
+                    override fun checkServerTrusted(
+                        chain: Array<X509Certificate>,
+                        authType: String
+                    ) {
+                    }
+
+                    override fun getAcceptedIssuers(): Array<X509Certificate> {
+                        return arrayOf()
+                    }
+                }
+            )
+
+            // Install the all-trusting trust manager
+            val sslContext = SSLContext.getInstance("SSL")
+            sslContext.init(null, trustAllCerts, SecureRandom())
+
+            // Create an ssl socket factory with our all-trusting manager
+            val sslSocketFactory = sslContext.socketFactory
+
+            val logger = HttpLoggingInterceptor()
+            val httpClient: OkHttpClient.Builder = OkHttpClient.Builder()
+
+            with(httpClient) {
+                logger.setLevel(HttpLoggingInterceptor.Level.HEADERS)
+                logger.setLevel(HttpLoggingInterceptor.Level.BODY)
+                addInterceptor(logger)
+                addInterceptor(authInterceptor)
+                sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+                hostnameVerifier { hostname: String?, session: SSLSession? -> true }
+            }
+            httpClient.addInterceptor { chain: Interceptor.Chain ->
+                val request: Request = chain.request().newBuilder()
+                    .header("UserBody-Agent", "android")
+                    .addHeader("Content-Type", "application/json")
+                    .build()
+                chain.proceed(request)
+            }
+
+            httpClient.build()
+        } catch (e: Exception) {
+            throw RuntimeException(e)
         }
-        httpClient.addInterceptor { chain: Interceptor.Chain ->
-            val request: Request = chain.request().newBuilder()
-                .header("UserBody-Agent", "android")
-                .addHeader("Content-Type", "application/json")
-                .build()
-            chain.proceed(request)
-        }
-        return httpClient.build()
     }
 
     @ViewModelScoped
--- data/src/main/java/com/sullivan/signear/data/remote/ApiService.kt
+++ data/src/main/java/com/sullivan/signear/data/remote/ApiService.kt
@@ -7,7 +7,7 @@ import retrofit2.http.*
 interface ApiService {
 
     companion object {
-        const val BASE_URL = "http://49.50.166.181:8088/"
+        const val BASE_URL = "https://49.50.166.181:8088/"
     }
 
     @GET("customer/check")
